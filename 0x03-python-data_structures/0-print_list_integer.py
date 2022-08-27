e2c97dccb1d1d9c6115b1f50ce7cc1816f7a764e
Oghenero Ologe <59165938+Nero2005@users.noreply.github.com>
2022-08-27 13:19:29 +0100

    Update 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     if my_list:
-        for item in my_list:
-            print("{:d}".format(item))
+        my_list.reverse()
+        for int in my_list:
+            print('{:d}'.format(int))
