ee8675fdb64440913a1953e769a237ecbf96d178
Oghenero Ologe <nologe37@gmail.com>
2022-08-31 04:40:51 -0700

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-	new_matrix = []
-	for i in matrix:
-		new_matrix.append([])
-		for j in i:
-			new_matrix[-1].append(j*j)
-	return new_matrix
+    new_matrix = []
+    for i in matrix:
+    	new_matrix.append([])
+    	for j in i:
+            new_matrix[-1].append(j*j)
+    return new_matrix
