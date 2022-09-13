d6823a60d6cf2ee7d014ae3dbee7ca26105a0b46
Oghenero Ologe <59165938+Nero2005@users.noreply.github.com>
2022-09-13 08:34:01 +0100

    task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -28,9 +28,7 @@ class Square:
 
     def my_print(self):
         if self.size == 0:
-            print()
+            print("")
             return
         for i in range(self.size):
-            for j in range(self.size):
-                print("#", end="")
-            print()
+            print("#" * self.size)
