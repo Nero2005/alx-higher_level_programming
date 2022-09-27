5d9142748cd0ebf7d7af43104f763010d5f41634
Oghenero Ologe <59165938+Nero2005@users.noreply.github.com>
2022-09-27 11:57:03 +0100

    task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Module 3 yh."""
+
+
+def append_write(filename="", text=""):
+    """ a function that appends a string to a text file (UTF8)
+    and returns the number of characters written."""
+    with open(filename, "a") as f:
+        return f.write(text)
