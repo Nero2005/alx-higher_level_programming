167352dbf7b2043eb12aaba3d5a8efdd75b24224
Oghenero Ologe <nologe37@gmail.com>
2022-11-02 13:40:43 +0100

    chaged 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,5 +2,6 @@
 -- lists all rows of a database that have one column in common
 SELECT tv_shows.title FROM tv_shows 
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
-WHERE tv_show_genres.genre_id = (SELECT id FROM tv_genres 
-WHERE `name` = 'Comedy') ORDER BY tv_shows.title ASC;
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id 
+WHERE tv_genres.name = 'Comedy' 
+ORDER BY tv_shows.title ASC;
