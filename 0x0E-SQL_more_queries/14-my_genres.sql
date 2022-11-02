5648657c826b200d2e85bf4de3668dbd48b59893
Oghenero Ologe <nologe37@gmail.com>
2022-11-02 13:34:09 +0100

    added tasks

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,6 @@
 -- lists all shows contained in the database hbtn_0d_tvshows
 -- lists all rows of a database that have one column in common
-SELECT tv_genres.name AS `name` FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_show_genres.show_id = (SELECT id FROM tv_shows WHERE `title` = 'Dexter') ORDER BY `name` ASC;
+SELECT tv_genres.name FROM tv_genres 
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
+WHERE tv_show_genres.show_id = (SELECT id FROM tv_shows 
+WHERE `title` = 'Dexter') ORDER BY tv_genres.name ASC;
