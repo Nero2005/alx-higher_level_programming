c9724798e803804e7481bcef53ada0d6b7334cf4
Oghenero Ologe <nologe37@gmail.com>
2022-11-29 05:33:17 -0800

    fix

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,5 +6,5 @@ const file2 = process.argv[3];
 const dest = process.argv[4];
 const file1Content = fs.readFileSync(file1).toString('utf-8');
 const file2Content = fs.readFileSync(file2).toString('utf-8');
-const file3Content = file1Content + '\n' + file2Content;
+const file3Content = file1Content + file2Content;
 fs.writeFileSync(dest, file3Content);
