643f729a32b8f13377a3607c7c04f62009668358
Oghenero Ologe <59165938+Nero2005@users.noreply.github.com>
2022-12-02 08:10:17 +0100

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
+"""takes in the name of a state as an argument and
+lists all cities of that state,
+using the database hbtn_0e_4_usa"""
 
 if __name__ == '__main__':
+
     import MySQLdb
     import sys
 
     db = MySQLdb.connect(host='localhost', port=3306,
-            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT cities.name\
