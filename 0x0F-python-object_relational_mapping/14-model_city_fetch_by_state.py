8e221f0521edd8a381aba13989a3306b53e048b9
Oghenero Ologe <59165938+Nero2005@users.noreply.github.com>
2022-12-02 08:33:04 +0100

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -9,7 +9,6 @@ if __name__ == "__main__":
     from model_city import City
     from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
