f51149d505f90cd2556c5b14fdf0d0553b1cdd71
Oghenero Ologe <59165938+Nero2005@users.noreply.github.com>
2022-12-16 08:45:29 +0100

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
+"""Takes in Github credentials (username and password) and uses the Github API
+to display an id"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    r = requests.get('https://api.github.com/user',
+                     auth=(sys.argv[1], sys.argv[2]))
+    if r.status_code >= 400:
+        print('None')
+    else:
+        print(r.json().get('id'))
