0d07eec3828031eb24456c60bbbb5ec11d22b77b
Oghenero Ologe <59165938+Nero2005@users.noreply.github.com>
2023-01-18 10:08:58 +0100

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+try {
+  fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+} catch (err) {
+  console.log(err);
+}
